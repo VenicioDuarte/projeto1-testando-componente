6723a6559c8b821e2442b9781a17720d9d68f5e2
Venicio duarte <151862005+VenicioDuarte@users.noreply.github.com>
2025-03-15 14:52:22 -0300

    Update Counter.test.js

--- src/components/Counter.test.js
+++ src/components/Counter.test.js
@@ -1,4 +1,3 @@
-// src/components/Counter.test.js
 import React from 'react'; // Importar o React
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
